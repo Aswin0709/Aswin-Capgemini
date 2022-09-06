3f48ab21c3a32bdcae6d38dad91a208e75a1f1f1
Aswin <aswinku2000@gmail.com>
2022-09-06 21:17:45 +0530

    Adding Assignment 2

---
+++ C#/LitwareLib (Assignment 2)/Mainapp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using LitwareLib;
+class EmployeeManagement
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter number of employees to be entered");
+        int n = int.Parse(Console.ReadLine());
+        Employee[] e = new Employee[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Enter Employee no of employee {0}", i + 1);
+      
+            int EmpNo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter name of Employee {0}", i + 1);
+            String EmpName = Console.ReadLine();
+            Console.WriteLine("Enter salary of Employee {0}", i + 1);
+            double Salary  = double.Parse(Console.ReadLine());
+            e[i] = new Employee(EmpNo, EmpName, Salary);
+            Console.WriteLine("Gross salary of employee {0} is {1}", i + 1, e[i].GrossSalary);
+        }
+
+    }
+}
